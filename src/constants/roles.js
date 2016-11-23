618afa1e43a1a76ab2cef7c13721f82303ebbb40
peidt <patceidt@gmail.com>
2016-11-23 14:02:49 -0500

    add rolesToText function for roles constants

--- src/constants/roles.js
+++ src/constants/roles.js
@@ -6,3 +6,24 @@ export const ROLE_PERCIVAL = 'ROLE_PERCIVAL'
 export const ROLE_MORGANA = 'ROLE_MORGANA'
 export const ROLE_MORDRED = 'ROLE_MORDRED'
 export const ROLE_OBERON = 'ROLE_OBERON'
+
+export function roleToText(role) {
+    switch(role) {
+        case ROLE_GOOD:
+            return 'Loyal Servant of Arthur'
+        case ROLE_EVIL:
+            return 'Minion of Mordred'
+        case ROLE_MERLIN:
+            return 'Merlin'
+        case ROLE_ASSASSIN:
+            return 'Assassin'
+        case ROLE_PERCIVAL:
+            return 'Percival'
+        case ROLE_MORGANA:
+            return 'Morgana'
+        case ROLE_MORDRED:
+            return 'Mordred'
+        case ROLE_OBERON:
+            return 'Oberon'
+    }
+}
